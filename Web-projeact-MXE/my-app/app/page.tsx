a6d279aca8ee834226556ae1b90c45ca99d5f8a5
MixerEmpire <mix254819dcmarvle@gmail.com>
2026-01-18 02:24:10 +0700

    Add files via upload

---
+++ Web-projeact-MXE/my-app/app/page.tsx
@@ -0,0 +1,14 @@
+function HomePage() {
+    return (
+        <div>
+            <div className="text-center mt-5 p-3 border -10">
+                <h1 className="text -3xl bg-blue-500">ยินดีต้อนรับสู่แอพของฉัน</h1>
+                <p className="text 2xl m-2">นี่คือ Next.js ที่ฉันเรียนเพื่อรู้</p>
+            </div>
+<div className="text-center text-2xl mt-6 text-blue-500">
+<h1>นี่คือหน้าของฉัน</h1>
+</div>
+        </div>
+    );
+}
+export default HomePage;
